740a0d1d97845d874a2bb47b08a107e8bb356665
Mickey <micksheridan.24@gmail.com>
2019-05-03 12:12:21 -0400

    basic layout demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import UnitList from "./containers/UnitList";
+import ProductionList from "./containers/ProductionList";
+import BuildMenu from "./containers/BuildMenu";
+import BuildSummary from "./containers/BuildSummary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allUnits: [],
+      build: [],
+    };
+  }
+  componentDidMount() {
+    this.fetchUnits();
+  }
+
+  fetchUnits = async () => {
+    const resp = await fetch("http://localhost:3000/units");
+    const units = await resp.json();
+    this.setState({ allUnits: units });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2 className="ui header" style={{ margin: "0.5em" }}>
+          <div className="content">Empires Macro-Manager</div>
+        </h2>
+        <div className="ui segment" style={{ margin: "0.5em", minHeight: "80vh" }}>
+          <div className="ui grid">
+            <div className="row" style={{ padding: "0em" }}>
+              <NavBar />
+            </div>
+            <div className="row" style={{ padding: "0em" }}>
+              <div className="column" style={{ flex: 4 }}>
+                <div className="ui segment" style={{ border: "0px solid red", boxShadow: "none", margin: "0.5em" }}>
+                  <Route exact path="/menu/:listType" render={rProps => <UnitList units={this.state.allUnits} {...rProps} />} />
+                  <Route exact path="/builds" render={() => <BuildMenu />} />
+                </div>
+              </div>
+
+              <BuildSummary build={this.state.build} />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
