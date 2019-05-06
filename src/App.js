f49d09015c82194ef79625ef1e4bd30da732d499
Mickey <micksheridan.24@gmail.com>
2019-05-06 11:54:49 -0400

    build menu, build fetches

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import UnitList from "./containers/UnitList";
 import BuildMenu from "./containers/BuildMenu";
-import BuildSummary from "./containers/BuildSummary";
-
 import "./App.css";
 
 class App extends Component {
@@ -21,12 +19,19 @@ class App extends Component {
     this.initialRequests();
   }
 
+  getCurrentBuild() {
+    return this.state.builds[this.state.currentBuild];
+  }
+  setCurrentBuild(id) {
+    this.setState({ currentBuild: this.state.builds.indexOf(this.state.builds.find(b => b.id === id)) });
+  }
+
   setImage = unit => {
-    let string = unit.name.toLowerCase();
-    string = string.replace(/ /g, "_");
+    let str = unit.name.toLowerCase();
+    str = str.replace(/ /g, "_");
     let ret = "";
     try {
-      ret = require(`./icons/${string}.png`);
+      ret = require(`./icons/${str}.png`);
     } catch (error) {
       if (unit.unit_type === "Technology") {
         ret = require("./icons/unique_tech_1.png");
@@ -35,10 +40,12 @@ class App extends Component {
     unit.image = ret;
   };
   initialRequests = async () => {
+    console.trace("initialRequests() called");
     await this.fetchUnits();
     await this.fetchBuilds();
   };
   fetchUnits = async () => {
+    // console.trace("fetchUnits() called");
     const resp = await fetch("http://localhost:3000/units");
     const units = await resp.json();
     units.forEach(unit => {
@@ -47,11 +54,13 @@ class App extends Component {
     this.setState({ allUnits: units });
   };
   fetchBuilds = async () => {
+    // console.trace("fetchBuilds() called");
     const resp = await fetch("http://localhost:3000/builds");
     const builds = await resp.json();
-    this.setState({ builds: builds }, () => console.log(this.state));
+    this.setState({ builds: builds });
   };
   fetchBuild = async id => {
+    // console.trace("fetchBuild() called");
     const req = await fetch(`http://localhost:3000/builds/${id}`);
     const build = await req.json();
     const index = this.state.builds.indexOf(this.state.builds.find(b => b.id === id));
@@ -60,58 +69,93 @@ class App extends Component {
     this.setState({ builds: [...builds] });
   };
 
-  //Needs to be brought to date
-  reorderBuild = (unit, unitIndex, targetIndex) => {
-    let newOrder = [...this.state.build];
-    newOrder.splice(targetIndex + 1, 0, unit);
-    if (targetIndex >= unitIndex) {
-      newOrder.splice(unitIndex, 1);
-    } else {
-      newOrder.splice(unitIndex + 1, 1);
-    }
-    this.setState({ build: newOrder });
+  reorderBuild = async (unit, unitIndex, targetIndex) => {
+    // console.trace("reorderBuild() called");
+    const req = await fetch(`http://localhost:3000/builds/rearrange/${this.getCurrentBuild().id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ unitIndex: unitIndex, targetIndex: targetIndex }),
+    });
+    await req.json();
+    this.fetchBuild(this.getCurrentBuild().id);
   };
 
-  //Needs to be brought to date
+  newBuild = async (name, description) => {
+    const req = await fetch("http://localhost:3000/builds", {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body: JSON.stringify({ name: name, description: description }),
+    });
+    const resp = await req.json();
+    if (resp) {
+      await this.fetchBuilds();
+      this.setCurrentBuild(resp.id);
+    }
+  };
+
   addUnit = async u => {
+    // console.trace("addUnit() called");
     let unit = { ...u };
     const unitId = unit.id;
-    const buildId = this.state.builds[this.state.currentBuild].id;
+    const buildId = this.getCurrentBuild().id;
 
     const req = await fetch("http://localhost:3000/build_units", {
       method: "POST",
       headers: { "Content-Type": "Application/json" },
       body: JSON.stringify({ unit_id: unitId, build_id: buildId }),
     });
-    const newEntry = await req.json();
-    console.log(newEntry);
+    await req.json();
     this.fetchBuild(buildId);
+  };
+
+  handleSelectBuild = e => {
+    e.preventDefault();
+    const build = this.state.builds.find(b => b.id === parseInt(e.target.value));
+
+    this.setState({ currentBuild: this.state.builds.indexOf(build) });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div style={{ maxHeight: "100vh" }}>
         <h2 className="ui header" style={{ margin: "0.5em" }}>
           <div className="content">Empires Macro-Manager</div>
         </h2>
-        <div className="ui segment" style={{ margin: "0.5em", minHeight: "80vh" }}>
+        <div className="ui segment" style={{ margin: "0.5em", minHeight: "80%" }}>
           <div className="ui grid">
             <div className="row" style={{ padding: "0em" }}>
               <NavBar />
             </div>
             <div className="row" style={{ padding: "0em" }}>
-              <div className="column" style={{ flex: 4 }}>
-                <div className="ui segment" style={{ border: "0px solid red", boxShadow: "none", margin: "0.5em" }}>
-                  <Route exact path="/menu/:listType" render={rProps => <UnitList handleAddUnit={this.addUnit} units={this.state.allUnits} {...rProps} />} />
-                  <Route exact path="/builds" render={() => <BuildMenu />} />
-                </div>
-              </div>
-
-              <BuildSummary reorder={this.reorderBuild} build={this.state.builds[this.state.currentBuild].units} />
+              <Route
+                exact
+                path="/menu/:listType"
+                render={rProps => (
+                  <UnitList
+                    reorder={this.reorderBuild}
+                    build={this.getCurrentBuild().units}
+                    handleAddUnit={this.addUnit}
+                    units={this.state.allUnits}
+                    {...rProps}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/builds"
+                render={() => (
+                  <BuildMenu
+                    handleSelectBuild={this.handleSelectBuild}
+                    newBuild={this.newBuild}
+                    current={this.state.currentBuild}
+                    builds={this.state.builds}
+                  />
+                )}
+              />
             </div>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
