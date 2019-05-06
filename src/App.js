96e57ef4316ec8769b1f7112df28d143ba5e58e4
Mickey <micksheridan.24@gmail.com>
2019-05-05 20:14:34 -0400

    started integration of backend build model

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import UnitList from "./containers/UnitList";
-import ProductionList from "./containers/ProductionList";
 import BuildMenu from "./containers/BuildMenu";
 import BuildSummary from "./containers/BuildSummary";
 
@@ -13,21 +12,80 @@ class App extends Component {
     super();
     this.state = {
       allUnits: [],
-      build: [],
+      buildCounter: 0,
+      builds: [{ name: "", description: "", units: [] }],
+      currentBuild: 0,
     };
   }
   componentDidMount() {
-    this.fetchUnits();
+    this.initialRequests();
   }
 
+  setImage = unit => {
+    let string = unit.name.toLowerCase();
+    string = string.replace(/ /g, "_");
+    let ret = "";
+    try {
+      ret = require(`./icons/${string}.png`);
+    } catch (error) {
+      if (unit.unit_type === "Technology") {
+        ret = require("./icons/unique_tech_1.png");
+      }
+    }
+    unit.image = ret;
+  };
+  initialRequests = async () => {
+    await this.fetchUnits();
+    await this.fetchBuilds();
+  };
   fetchUnits = async () => {
     const resp = await fetch("http://localhost:3000/units");
     const units = await resp.json();
+    units.forEach(unit => {
+      this.setImage(unit);
+    });
     this.setState({ allUnits: units });
   };
+  fetchBuilds = async () => {
+    const resp = await fetch("http://localhost:3000/builds");
+    const builds = await resp.json();
+    this.setState({ builds: builds }, () => console.log(this.state));
+  };
+  fetchBuild = async id => {
+    const req = await fetch(`http://localhost:3000/builds/${id}`);
+    const build = await req.json();
+    const index = this.state.builds.indexOf(this.state.builds.find(b => b.id === id));
+    const builds = [...this.state.builds];
+    builds[index] = build;
+    this.setState({ builds: [...builds] });
+  };
 
-  addUnit = unit => {
-    this.setState({ build: [...this.state.build, unit] }, () => console.log(this.state));
+  //Needs to be brought to date
+  reorderBuild = (unit, unitIndex, targetIndex) => {
+    let newOrder = [...this.state.build];
+    newOrder.splice(targetIndex + 1, 0, unit);
+    if (targetIndex >= unitIndex) {
+      newOrder.splice(unitIndex, 1);
+    } else {
+      newOrder.splice(unitIndex + 1, 1);
+    }
+    this.setState({ build: newOrder });
+  };
+
+  //Needs to be brought to date
+  addUnit = async u => {
+    let unit = { ...u };
+    const unitId = unit.id;
+    const buildId = this.state.builds[this.state.currentBuild].id;
+
+    const req = await fetch("http://localhost:3000/build_units", {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body: JSON.stringify({ unit_id: unitId, build_id: buildId }),
+    });
+    const newEntry = await req.json();
+    console.log(newEntry);
+    this.fetchBuild(buildId);
   };
 
   render() {
@@ -49,7 +107,7 @@ class App extends Component {
                 </div>
               </div>
 
-              <BuildSummary build={this.state.build} />
+              <BuildSummary reorder={this.reorderBuild} build={this.state.builds[this.state.currentBuild].units} />
             </div>
           </div>
         </div>
